a17240c2184ae307a9edc35e2c0ef76753838baf
VladislavFesko <42113564+VladislavFesko@users.noreply.github.com>
2019-12-27 23:03:02 +0300

    Add files via upload

--- EraConvert/to_XEX/__init__.py
+++ EraConvert/to_XEX/__init__.py
@@ -19,38 +19,38 @@ __all__ = [
     'convert_EEEp3',
     ]
 def convert_ATE(year):
-    print("2E" + year)
+    return "2E" + year
 def convert_Infinities(year):
-    print("3E" + year)
+    return "3E" + year
 def convert_Trids(year):
-    print("4E" + year)
+    return "4E" + year
 def convert_Tetix(year):
-    print("5E" + year)
+    return "5E" + year
 def convert_Petix(year):
-    print("6E" + year)
+    return "6E" + year
 def convert_Ectix(year):
-    print("7E" + year)
+    return "7E" + year
 def convert_Zettix(year):
-    print("8E" + year)
+    return "8E" + year
 def convert_Yottix(year):
-    print("9E" + year)
+    return "9E" + year
 def convert_Xenux(year):
-    print("10E" + year)
+    return "10E" + year
 def convert_YOV(year):
-    print("7000000000E" + year)
+    return "7000000000E" + year
 def convert_YOC(year):
-    print("8000000008E" + year)
+    return "8000000008E" + year
 def convert_YOH(year):
-    print("6.66e+666E" + year)
+    return "6.66e+666E" + year
 def convert_xATE(integer, year):
-    print(integer + "E" + year)
+    return integer + "E" + year
 def convert_MATE(year):
-    print("13.8e+3000000003E" + year)
+    return "13.8e+3000000003E" + year
 def convert_EEE(year):
-    print("13.8e+2999999999999999997-1" + year)
+    return "13.8e+2999999999999999997-1" + year
 def convert_EEEp(year):
-    print("13.8e+3000000000000000000-1E" + year)
+    return "13.8e+3000000000000000000-1E" + year
 def convert_EEEp2(year):
-    print("13.8e+3000000000000000003-1E" + year)
+    return "13.8e+3000000000000000003-1E" + year
 def convert_EEEp3(year):
-    print("13.8e+3000000000000000006-1E" + year)
+    return "13.8e+3000000000000000006-1E" + year
